73685a99ae672185bd57b1dc974e2b68be18a564
Roberto <robertopaiva26@outlook.com>
2021-02-09 12:18:21 -0300

    Melhora na logica do filter

--- main.js
+++ main.js
@@ -1,13 +1,12 @@
 const url = 'http://files.cod3r.com.br/curso-js/funcionarios.json'
 const axios = require('axios')
 
-const br = x => x.pais == 'Brazil'
-const genero = y => y.genero == "M"
-const salario = z => z.salario >= 15000
+const brGSalario = x => x.pais == 'Brazil' && x.genero == "M" && x.salario >= 15000
+
 
 axios.get(url).then(response => {
     const funcionarios = response.data
-    const brMSalario = funcionarios.filter(br).filter(genero).filter(salario)
+    const brMSalario = funcionarios.filter(brGSalario)
     console.log(brMSalario)
 
     console.log("A quantidade de Brasileiros do sexo Masculino com salario acima de 15000 são", brMSalario.length)
